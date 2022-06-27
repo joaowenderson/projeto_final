c29a148d7d178fd69c8dfe0621a68cefb9f5c9da
joaowenderson <joaowenderson17@gmail.com>
2022-06-27 13:54:29 -0300

    Initial commit

---
+++ src/app/consulta-alunos/alunos.ts
@@ -0,0 +1,12 @@
+export class alunos{
+  nome: string;
+  matricula: number;
+  curso: string;
+  notas: number;[];
+  constructor(nm: string, matri: number, curso; String, notas: number[]) {
+    this.nome=nm;
+    this.matricula= matri;
+    this.curso = curso;
+    this.notas= notas;
+  }
+}
